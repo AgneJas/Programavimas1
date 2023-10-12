b4720fef82beda0015efa102091d43c5c607d3fd
Agnė <Agn@DESKTOP-OEQQP3F>
2023-10-12 21:21:19 +0300

    Add project files.

---
+++ 01Kintamieji/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01Kintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+            Console.WriteLine("Apie mane:");
+            var vardas = "Agne";
+            Console.WriteLine("Vardas " + vardas);
+            string pavarde = "Jas";
+            Console.WriteLine("Pavarde " + pavarde);
+            //
+            int amzius = 35;
+            Console.WriteLine("Amzius " + amzius);
+            int ugis = 175;
+            Console.WriteLine("Ugis " + ugis);
+            float svoris = 8.5f;
+            Console.WriteLine("Svoris " + svoris);
+            string aukstojimokykla = "Vilniaus Universitetas";
+            Console.WriteLine("Aukstoji mokykla " + aukstojimokykla);
+            int akademinesgrupeskodas = 5;
+            Console.WriteLine("Akademines grupes kodas " + akademinesgrupeskodas);
+            string kursas = "Dieninis";
+            Console.WriteLine("Kursas " + kursas);
+            string studijosprogramospavadinimas = "Programuotojai";
+            Console.WriteLine("Studijos programos pavadinimas " + studijosprogramospavadinimas);
+            int atsiskaitytukredituskaicius = 205;
+            Console.WriteLine("Atsiskaitytu kreditu skaicius " + atsiskaitytukredituskaicius);
+
+          
+
+
+        }
+    }
+}
