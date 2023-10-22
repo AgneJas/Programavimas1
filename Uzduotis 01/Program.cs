570bbe4551c008f02386d20f43c7f2dea1959286
AgneJas <agne.jokubauskaite@gmail.com>
2023-10-22 12:53:58 +0300

    Uzduotis01

--- Uzduotis 01/Program.cs
+++ Uzduotis 01/Program.cs
@@ -8,19 +8,26 @@ namespace Uzduotis_01
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            /*
-             * Parašykite programą, kuri išvestų žodį “Labas” ir jūsų vardą toje pačioje eilutėje.
-             */
-
             string name = "Agne";
-            Console.WriteLine("Labas" + name);
+            Console.WriteLine("Labas, " + name);
             Console.WriteLine();
+
 
 
             /*
              * Susikurkite kintamąjį žmogaus amžiui nusakyti, išveskite į ekraną tekstą su
- šiuo amžiumi: “Įvestas amžius: {amžius}”. Tarp {} nurodytas kintamasis, kurio
- reikšmė toje vietoje turi būti įstatyta.
+             šiuo amžiumi: “Įvestas amžius: {amžius}”. Tarp {} nurodytas kintamasis, kurio
+             reikšmė toje vietoje turi būti įstatyta.
+            */
+            int amzius = 35;
+            Console.WriteLine("Ivestas amzius: " + amzius);
+            Console.WriteLine();
+
+            /*
+             * Susikurkite skaičiaus kintamąjį, kurį išveskite 
+             penkis kartus toje pačioje
+            eilutėje su tarpais tarp šių skaičių 
+            (skaičius - 25, rezultatas - 25 25 25 25 25).
             */
 
             int skaicius = 25;
@@ -30,16 +37,8 @@ namespace Uzduotis_01
             Console.Write(skaicius);
             Console.WriteLine(skaicius);
 
-            /*
-             * Susikurkite skaičiaus kintamąjį, kurį išveskite 
-             penkis kartus toje pačioje
-            eilutėje su tarpais tarp šių skaičių 
-            (skaičius - 25, rezultatas - 25 25 25 25 25).
-            */
-
             Console.WriteLine($"{skaicius} {skaicius} {skaicius} {skaicius}");
             Console.WriteLine();
-
         }
     }
 }
