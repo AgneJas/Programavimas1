7505c35ee1febf479d323fc21030f58e1592a66a
AgneJas <agne.jokubauskaite@gmail.com>
2023-10-26 20:23:57 +0300

    Uzduotis 08

--- Uzduotis 08/Program.cs
+++ Uzduotis 08/Program.cs
@@ -6,21 +6,14 @@ namespace Uzduotis_08
     {
         static void Main(string[] args)
         {
-
             /*
              Liepkite vartotojui įvesti bet kokį sveikąjį skaičių. 
              Išveskite šio skaičiaus daugybos lentelę nuo 1 iki 5. Pvz.: 2 * 1 = 2 … 2 * 5 = 10*/
 
             Console.WriteLine("Iveskite bet koki sveikaji skaiciu");
             var skaicius = Convert.ToInt32(Console.ReadLine());
-            int daugiklis = 0;
+            int daugiklis = 1;
             Console.WriteLine($"Skaiciaus {skaicius} daugybos lentele:");
-            Console.WriteLine($"{skaicius} * {daugiklis} = {skaicius * daugiklis++}");
-            Console.WriteLine($"{skaicius} * {daugiklis} = {skaicius * daugiklis++}");
-            Console.WriteLine($"{skaicius} * {daugiklis} = {skaicius * daugiklis++}");
-            Console.WriteLine($"{skaicius} * {daugiklis} = {skaicius * daugiklis++}");
-            Console.WriteLine($"{skaicius} * {daugiklis} = {skaicius * daugiklis++}");
-            Console.WriteLine($"{skaicius} * {daugiklis} = {skaicius * daugiklis++}");
             Console.WriteLine($"{skaicius} * {daugiklis} = {skaicius * daugiklis++}");
             Console.WriteLine($"{skaicius} * {daugiklis} = {skaicius * daugiklis++}");
             Console.WriteLine($"{skaicius} * {daugiklis} = {skaicius * daugiklis++}");
@@ -28,18 +21,35 @@ namespace Uzduotis_08
             Console.WriteLine($"{skaicius} * {daugiklis} = {skaicius * daugiklis++}");
             Console.WriteLine();
 
+            /* Liepkite vartotojui įvesti tris skaičius. Į ekraną išveskite šiuos skaičius ir jų vidurkį. Pvz.:
+             Įvestų skaičių 4, 5, 8 vidurkis: 5,7.*/
 
+            Console.WriteLine("Iveskite tris skaicius");
+            int number1, number2, number3;
+            number1 = Convert.ToInt32(Console.ReadLine());
+            number2 = Convert.ToInt32(Console.ReadLine());
+            number3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Jus ivedete: {number1}, {number2}, {number3}");
+            double average = (number1 + number2 + number3) / 3;
+            Console.WriteLine($"average = {average}");
+            Console.WriteLine();
 
+            /*
+            Liepkite įvesti tris skaičius. Išveskite įvestus skaičius, jų kvadratus, 
+            juos pakeltus trečiuoju laipsniu, taip pat tuos skaičius, padalintus iš dviejų.*/
 
+            Console.WriteLine($"Jus ivedete: {number1}, {number2}, {number3}");
+            Console.WriteLine($" Skaicius {number1} kvadratu: {number1}^2 = {number1 * number1}");
+            Console.WriteLine($" Skaicius {number2} kvadratu: {number2}^2 = {number2 * number2}");
+            Console.WriteLine($" Skaicius {number3} kvadratu: {number3}^2 = {number3 * number3}");
+            Console.WriteLine();
 
+            Console.WriteLine($" Skaicius {number1} treciuoju laipsniu: {number1}^3 = {number1 * number1 * number1}");
+            Console.WriteLine($" Skaicius {number2} treciuoju laipsniu: {number2}^3 = {number2 * number2 * number2}");
+            Console.WriteLine($" Skaicius {number2} treciuoju laipsniu: {number2}^3 = {number3 * number3 * number3}");
 
-
-
-           /* Liepkite vartotojui įvesti tris skaičius. Į ekraną išveskite šiuos skaičius ir jų vidurkį. Pvz.:
-Įvestų skaičių 4, 5, 8 vidurkis: 5,7.
-Liepkite įvesti tris skaičius. Išveskite įvestus skaičius, jų kvadratus, juos pakeltus trečiuoju laipsniu, taip pat tuos skaičius, padalintus iš dviejų.*/
-           
-
+            Console.WriteLine($"Skaiciai padalinti is 2: {number1}, {number2}, {number3} = {number1 / 2}, {number2 / 2}, {number3 /2}");
+            Console.WriteLine();
         }
     }
 }
