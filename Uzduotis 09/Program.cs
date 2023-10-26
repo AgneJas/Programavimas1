a17b70ffc9992a71e9d11f0fd87564bb98f35047
AgneJas <agne.jokubauskaite@gmail.com>
2023-10-26 20:47:21 +0300

    Uzduotis 09

---
+++ Uzduotis 09/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Uzduotis_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Liepkite vartotojui įvesti savo amžių. Jei amžius yra 18 arba daugiau, tuomet į ekraną išveskite, kad “jūs galite balsuoti”.*/
+
+            Console.WriteLine("Iveskite savo amziu: ");
+            var a = 18;
+            if (a >= 18)
+            Console.WriteLine("Jus galite balsuoti");
+            Console.WriteLine("Ivestas amzius: ");
+                Console.WriteLine();
+
+
+
+
+
+
+
+
+
+            /*Liepkite vartotojui įvesti bet kokį skaičių. Parašykite šias atskiras if sąlygas:
+            Patikrinti, ar skaičius neigiamas, jei taip - aprėkti vartotoją :)
+            Patikrinti, ar skaičius teigiamas, jei taip - pasveikinti vartotoją;
+            Patikrinti, ar skaičius lyginis, jei taip - išvesti “taip, skaičius yra lyginis”;
+            Patikrinti, ar skaičius dalinasi iš 4, jei taip - išvesti “taip, dalinasi iš 4”;
+            Patikrinti, ar skaičius yra didesnis nei 10, jei taip - išvesti “taip, skaičius yra didesnis už 10”.
+            Liepkite vartotojui įvesti penkis  du savo pažymius, raskite šių pažymių vidurkį, patikrinkite, ar vidurkis yra lygus arba didesnis už 5, jei taip, išveskite “Valio!!!”.
+            */
+
+        }
+    }
+}
