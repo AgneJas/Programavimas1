c6b951b9bf9f7979133c04ce2c4778075643cb69
AgneJas <agne.jokubauskaite@gmail.com>
2023-11-06 18:09:39 +0200

    34 uzduotis

---
+++ 34 uzduotis/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34_uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
