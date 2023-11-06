c5fe44d2931bf37b8ec196a3268af853dd884509
AgneJas <agne.jokubauskaite@gmail.com>
2023-11-06 19:25:13 +0200

    29 uzduotis

--- 29 uzduotis/Program.cs
+++ 29 uzduotis/Program.cs
@@ -9,7 +9,32 @@ namespace _29_uzduotis
     internal class Program
     {
         static void Main(string[] args)
+        {            /*
+             * Parašykite metodą, kuris pasisveikintų su vartotoju.
+             * Vartotojas turi įvesti savo vardą, amžių ir hobio pavadinimą.
+             * Visi šie duomenys yra duodami metodui, 
+             * kuris visą šią informaciją išveda formatuotai:
+             */
+
+            string vardas = VartotojoIvedimas("Įrašykite savo vardą");
+            int amzius = Convert.ToInt32(VartotojoIvedimas("Įrašykite savo amžių"));
+            string hobis = VartotojoIvedimas("Įrašykite savo hobį");
+
+            PasisveikinimasSuVartotoju(vardas, amzius, hobis);
+        }
+
+        private static string VartotojoIvedimas(string zinute)
         {
+            Console.Write($"{zinute}: ");
+            return Console.ReadLine();
+        }
+
+        private static void PasisveikinimasSuVartotoju(string vardas, int amzius, string hobis)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Labas, tavo vardas {vardas}, tau {amzius} metų.");
+            Console.WriteLine($"Tavo mylimiausias hobis yra {hobis}.");
+            Console.WriteLine();
         }
     }
 }
