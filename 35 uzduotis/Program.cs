6a2ec2da8c0deb89361c524d864f2b4ccb0a5bd6
AgneJas <agne.jokubauskaite@gmail.com>
2023-11-06 19:26:00 +0200

    35 uzduotis

--- 35 uzduotis/Program.cs
+++ 35 uzduotis/Program.cs
@@ -9,7 +9,35 @@ namespace _35_uzduotis
     internal class Program
     {
         static void Main(string[] args)
+        {            /*
+             * Sukurti void tipo metodą, kuriam paduosim masyvą.
+             * 
+             * Metodas turi išvesti masyvo sumą 
+             *      (sumai skaičiuoti naudoti for ciklą ir .Sum()  funkciją. 
+             *      Tuomet metodas išves į konsolę 2 tokias pačias sumas
+             *      suskaičiuotas skirtingais būdais)
+             * 
+             * Išsikviesti metodą.
+             */
+
+            int[] masyvas = { 5, 7, 8, 15, 20, 3, 5, 17 };
+
+            SkaiciuotiSuma(masyvas);
+        }
+
+        private static void SkaiciuotiSuma(int[] masyvas)
         {
+            int sumaCiklo = 0;
+
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                sumaCiklo += masyvas[i];
+            }
+
+            int sumaMasyvo = masyvas.Sum();
+
+            Console.WriteLine(sumaCiklo);
+            Console.WriteLine(sumaMasyvo);
         }
     }
 }
