712d5701a67241a1a724bc58d5a7df3419cf2cea
AgneJas <agne.jokubauskaite@gmail.com>
2023-11-06 19:25:38 +0200

    32 uzduotis

--- 32 uzduotis/Program.cs
+++ 32 uzduotis/Program.cs
@@ -9,7 +9,25 @@ namespace _32_uzduotis
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {            /* Paprašyti įvesti vartotojo vardą.
+            
+             * Sukurti string tipo metodą 
+             * kuris gražina frazę pvz „Sveiki, Mantas“.
+             
+             * Išvesti šią frazę į konsolę.
+             */
+
+            Console.Write("Įveskite savo vardą: ");
+            string vardas = Console.ReadLine();
+
+            string zinute = Pasisveikinimas(vardas);
+            Console.WriteLine(zinute);
         }
+
+        private static string Pasisveikinimas(string vardas)
+        {
+            return $"Sveiki, {vardas}";
+        }
+    }
     }
 }
