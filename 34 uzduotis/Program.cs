a04d06e112e2a2102d09770745706bd6d281bac8
AgneJas <agne.jokubauskaite@gmail.com>
2023-11-06 19:25:53 +0200

    34 uzduotis

--- 34 uzduotis/Program.cs
+++ 34 uzduotis/Program.cs
@@ -10,6 +10,39 @@ namespace _34_uzduotis
     {
         static void Main(string[] args)
         {
+            Console.Write("Įveskite pirmą sveikąjį skaičių: ");
+            string ivedimas = Console.ReadLine();
+            int skaicius1 = Convert.ToInt32(ivedimas);
+
+            Console.Write("Įveskite antrą sveikąjį skaičių: ");
+            ivedimas = Console.ReadLine();
+            int skaicius2 = Convert.ToInt32(ivedimas);
+
+            Console.Write("Kokį veiksmą norite atlikti? (daugyba / dalyba): ");
+            ivedimas = Console.ReadLine();
+
+            if (ivedimas == "daugyba")
+            {
+                Console.WriteLine($"Sandauga: {Daugyba(skaicius1, skaicius2)}");
+            }
+            else if (ivedimas == "dalyba")
+            {
+                Console.WriteLine($"Dalyba: {Dalyba(skaicius1, skaicius2)}");
+            }
+            else
+            {
+                Console.WriteLine($"veiksmo '{ivedimas}' nėra!");
+            }
+        }
+
+        private static double Dalyba(int skaicius1, int skaicius2)
+        {
+            return (double)skaicius1 / skaicius2;
+        }
+
+        private static int Daugyba(int skaicius1, int skaicius2)
+        {
+            return skaicius1 * skaicius2;
         }
     }
 }
