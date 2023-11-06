ea18f7c5379df8134b301048c2d9df5ba5ab21c7
AgneJas <agne.jokubauskaite@gmail.com>
2023-11-06 19:25:03 +0200

    28 uzduotis

--- 28 uzduotis/Program.cs
+++ 28 uzduotis/Program.cs
@@ -9,7 +9,17 @@ namespace _28_uzduotis
     internal class Program
     {
         static void Main(string[] args)
+        {            /*
+             * Aprašykite ir iškvieskite metodą, 
+             * kuris išvestų bet kokį jūsų pasirinktą tekstą.
+             */
+
+            ManoPasisveikinimas();
+        }
+
+        private static void ManoPasisveikinimas()
         {
+            Console.WriteLine("Labas");
         }
     }
-}
+    }
